4bed9311fed01af05e7b1a71e896278146d17d27
Paul Chapeau <paul.chapeau94@gmail.com>
2020-02-27 11:56:19 +0100

    more suitable design

--- app/javascript/components/search.js
+++ app/javascript/components/search.js
@@ -1,29 +1,36 @@
 const list = document.querySelector('#results');
 
-const insertMovies = (data) => {
-  var huge_list = [];
-  data.Search.slice(0,5).forEach((result) => {
-    const movie = `<li class="autocomplete-movies d-flex">
-      <img src="${result.Poster}" alt="" />
-      <a href="/movies/${result.imdbID}">${result.Title}</a>
-    </li>`;
-    huge_list.push(result.Title);
-    list.insertAdjacentHTML('beforeend', movie);
+
+// If list afficher
+if (list) {
+  const insertMovies = (data) => {
+    var huge_list = [];
+    data.Search.slice(0,5).forEach((result) => {
+      const movie = `
+      <a href="/movies/${result.imdbID}" class="link-without-style">
+        <li class="autocomplete-movies d-flex align-items-center">
+          <img src="${result.Poster}" alt="" />
+          <span>${result.Title} </span>
+        </li>
+      </a>`;
+      huge_list.push(result.Title);
+      list.insertAdjacentHTML('beforeend', movie);
+    });
+  };
+  const fetchMovies = (query) => {
+    fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+      .then(response => response.json())
+      .then(insertMovies);
+  };
+
+  fetchMovies(''); // on 1st page load
+
+  const form = document.querySelector('#search-form');
+  form.addEventListener('keyup', (event) => {
+    event.preventDefault();
+    list.innerHTML = '';
+    const input = document.querySelector('#search-input');
+    fetchMovies(input.value);
   });
-};
+}
 
-const fetchMovies = (query) => {
-  fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
-    .then(response => response.json())
-    .then(insertMovies);
-};
-
-fetchMovies(''); // on 1st page load
-
-const form = document.querySelector('#search-form');
-form.addEventListener('keyup', (event) => {
-  event.preventDefault();
-  list.innerHTML = '';
-  const input = document.querySelector('#search-input');
-  fetchMovies(input.value);
-});
