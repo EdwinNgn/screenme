0b13918e92ca93d3ad7b9c7f43b9f9d9aac857ff
Edwin Nguyen <edwin.nguyen@edhec.com>
2020-02-27 17:13:36 +0100

    not done but works

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
+require "open-uri"
 class Movie < ApplicationRecord
+  def get_details
+    id = "0"*(7 - self.omdb_id.to_s.length) + self.omdb_id.to_s
+    url = "http://www.omdbapi.com/?i=tt#{id}&apikey=adf1f2d7"
+    movie = JSON.parse(open(url).read)
+  end
 end
