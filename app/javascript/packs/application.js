45de7f73c0c1bf11014db64664753955cc3b70ef
Edwin Nguyen <edwin.nguyen@edhec.com>
2020-03-02 16:26:38 +0100

    search bar friends done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,18 @@ import "movies/mark_as_like";
 import "movies/mark_as_dislike";
 import "movies/mark_as_save";
 import "movies/mark_as_blacklist";
-import "components/step_two"
+import "components/step_two";
+
+
+import 'select2/dist/css/select2.css';
+
+import { initSelect2 } from '../components/search-friend';
+
+initSelect2();
+
+
+$('.select2').on('select2:select', function (e) {
+    window.location.href = `/profiles/${e.params.data.id}`
+    var data = e.params.data;
+    console.log(data);
+});
