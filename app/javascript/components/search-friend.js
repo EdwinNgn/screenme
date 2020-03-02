569beec62e280c59975fb4364c2c06ddbfc9b135
Edwin Nguyen <edwin.nguyen@edhec.com>
2020-03-02 17:05:06 +0100

    js launched on profile only

--- app/javascript/components/search-friend.js
+++ app/javascript/components/search-friend.js
@@ -2,10 +2,12 @@
 import $ from 'jquery';
 import 'select2';
 
-const initSelect2 = () => {
+const profile = document.querySelector(".profile-content");
+
+if (profile) {
   $('.select2').select2();
+
+  $('.select2').on('select2:select', function (e) {
+      window.location.href = `/profiles/${e.params.data.id}`
+  });
 };
-
-
-
-export { initSelect2 };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,17 +8,5 @@ import "movies/mark_as_dislike";
 import "movies/mark_as_save";
 import "movies/mark_as_blacklist";
 import "components/step_two";
-
-
 import 'select2/dist/css/select2.css';
-
-import { initSelect2 } from '../components/search-friend';
-
-initSelect2();
-
-
-$('.select2').on('select2:select', function (e) {
-    window.location.href = `/profiles/${e.params.data.id}`
-    var data = e.params.data;
-    console.log(data);
-});
+import 'components/search-friend';
