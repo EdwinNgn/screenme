fa7b2d1ac61cdccf05be36da9fb805d161830cf2
Paul Chapeau <paul.chapeau94@gmail.com>
2020-03-03 15:42:08 +0100

    add waaarning icon

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,11 +2,15 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
     if @user == current_user
-      @movies = @user.recommended_movies
+      @movies = @user.recommended_movies.to_a
     else
-      @movies = @user.recommended_movies & current_user.recommended_movies
+      @movies = @user.recommended_movies.to_a & current_user.recommended_movies.to_a
     end
-    @movies = Movie.top(10) if @movies.blank?
+    Movie.top(10).each do |movie|
+      break if @movies.size == 10
+      next if  @movies.include?(movie)
+      @movies << movie
+    end
     @movie_details = @movies.map { |movie| movie.get_details }
   end
 end
