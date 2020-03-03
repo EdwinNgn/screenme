db3ec1e7094868868fec21fe2916d443887ec808
Paul Chapeau <paul.chapeau94@gmail.com>
2020-03-03 16:37:31 +0100

    permet de montrer les 3 reco d'un autre utilisateur si il en as

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,11 +4,12 @@ class ProfilesController < ApplicationController
     if @user == current_user
       @movies = @user.recommended_movies.to_a
     else
-      @movies = @user.recommended_movies.to_a & current_user.recommended_movies.to_a
+      @movies = @user.recommended_movies.first(3).to_a + (@user.recommended_movies.to_a & current_user.recommended_movies.to_a)
+      @movies = @movies.uniq
     end
-    Movie.top(10).each do |movie|
+    Movie.top(100).each do |movie|
       break if @movies.size == 10
-      next if  @movies.include?(movie)
+      next if  @movies.include?(movie) || @user.rated?(movie)
       @movies << movie
     end
     @movie_details = @movies.map { |movie| movie.get_details }
