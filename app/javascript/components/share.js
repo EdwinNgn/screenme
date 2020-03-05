927cc8624fbada30aa2e9e85b3137a4e905e32c6
Edwin Nguyen <edwin.nguyen@edhec.com>
2020-03-05 15:22:52 +0100

    debug

---
+++ app/javascript/components/share.js
@@ -0,0 +1,17 @@
+
+
+document.querySelector('.share-btn').addEventListener('click', () => {
+  document.querySelector('.select2').click()
+})
+
+
+// $(document).ready(
+//     function () {
+//         $(".select2").select2();
+
+//         $(".share-btn").click(function(){
+//             $(".select2").select2("open");
+//         });
+//     }
+
+// );
